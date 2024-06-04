052ad0de7eddca327f6b4b264d6f23eb41c22c2b
aymensellaouti <aymen.sellaouti@gmail.com>
2024-06-04 11:47:49 +0100

    exemple OnInit

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
   styleUrls: ['./color.component.css'],
 })
-export class ColorComponent {
+export class ColorComponent implements OnInit {
+  @Input()
   defaultColor = 'gray';
-  color = this.defaultColor;
+  color = '';
+  constructor() {}
+  ngOnInit(): void {
+    this.color = this.defaultColor;
+  }
   changeColor(newColor: HTMLInputElement ) {
     this.color = newColor.value;
     newColor.value = '';
