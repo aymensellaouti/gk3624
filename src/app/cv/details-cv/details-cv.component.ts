42c8e96ae4b562ac352406a81a740cae43938d89
aymensellaouti <aymen.sellaouti@gmail.com>
2024-06-05 11:56:33 +0100

    exo details cv et suppression

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Cv } from '../model/cv.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CvService } from '../services/cv.service';
+import { APP_ROUTES } from 'src/app/config/routes.config';
 
 
 @Component({
@@ -9,10 +12,23 @@ import { Cv } from '../model/cv.model';
 })
 export class DetailsCvComponent implements OnInit {
   cv: Cv | null = null;
-  constructor(
-
-  ) {}
+  acr = inject(ActivatedRoute);
+  cvService = inject(CvService);
+  router = inject(Router);
+  constructor() {}
 
   ngOnInit() {
+    const id = this.acr.snapshot.params['id'];
+    this.cv = this.cvService.getCvById(id);
+    if (!this.cv) {
+      this.router.navigate([APP_ROUTES.cv]);
+    }
+  }
+
+  delete() {
+    if (this.cv) {
+      this.cvService.deleteCv(this.cv);
+      this.router.navigate([APP_ROUTES.cv]);
+    }
   }
 }
--- src/app/cv/services/cv.service.ts
+++ src/app/cv/services/cv.service.ts
@@ -41,4 +41,33 @@ export class CvService {
   getCvs(): Cv[] {
     return this.cvs;
   }
+
+  /**
+   *
+   * Retourne un cv via son id
+   *
+   * @param id : number, l'id du cv à sélectionner
+   * @returns CV ou null si le cv n'est pas trouvé
+   *
+   */
+  getCvById(id: number): Cv | null {
+    return this.cvs.find(c => c.id == id) ?? null;
+  }
+
+  /**
+   *
+   * Retourne un boolean permettant de savoir si on a supprimé le cv ou pas
+   *
+   * @param cv: CV, le cv à supprimer
+   * @returns boolean
+   *
+   */
+  deleteCv(cv: Cv): boolean {
+    const index = this.cvs.indexOf(cv);
+    if (index > -1) {
+      this.cvs.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
 }
